1ee3c9df46e28d0d2c8d55a885602e633b65817e
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2025-12-05 20:57:40 +0100

    ninthCommit

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ function Welcome() {
     <>
       <div className="entire">
         <div className="welcome">
-          <h1 className="make">Welcome to Calculator App</h1>
+          <h1 className="make">Welcome to <span className="josh">Joshua's</span> Calculator App</h1>
           <p className="choose">Choose a mode </p>
         </div>
         <div className="images">
